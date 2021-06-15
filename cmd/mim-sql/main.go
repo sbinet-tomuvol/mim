30f1a4b3a7cfc2eb60a4fca42106c8317a5294a5
Sebastien Binet <binet@cern.ch>
2021-06-15 15:49:40 +0200

    cmd/mim-sql: add more tests

--- cmd/mim-sql/main.go
+++ cmd/mim-sql/main.go
@@ -99,6 +99,128 @@ func doQuery(db *conddb.DB, hrConfig string, difID int) error {
 			}
 		}
 	}
+	{
+		/*
+			+------------+----------------------+------+-----+---------+-------+
+			| Field      | Type                 | Null | Key | Default | Extra |
+			+------------+----------------------+------+-----+---------+-------+
+			| detector   | int(11)              | NO   | PRI | NULL    |       |
+			| identifier | int(11)              | NO   | PRI | NULL    |       |
+			| eda        | int(11)              | NO   | MUL | NULL    |       |
+			| iz         | tinyint(1) unsigned  | NO   |     | 0       |       |
+			| lv         | smallint(5) unsigned | NO   |     | 0       |       |
+			+------------+----------------------+------+-----+---------+-------+
+		*/
+		rows, err := db.QueryContext(ctx, "SELECT detector, identifier, eda, iz, lv from layers")
+		if err != nil {
+			return fmt.Errorf("could not get layers definition: %w", err)
+		}
+		defer rows.Close()
+
+		nrows := 0
+		for rows.Next() {
+			var (
+				det int32
+				id  int32
+				eda int32
+				iz  uint8
+				lv  uint32
+			)
+			err = rows.Scan(&det, &id, &eda, &iz, &lv)
+			if err != nil {
+				return fmt.Errorf("could not scan layers table: %w", err)
+			}
+			log.Printf(
+				"layer> id=%v, det=%v, eda=%v, iz=%v, lv=%v",
+				id, det, eda, iz, lv,
+			)
+			nrows++
+		}
+		if nrows == 0 {
+			log.Printf("empty layers table")
+		}
+	}
+	{
+		/*
+			+------------+-------------+------+-----+---------+----------------+
+			| Field      | Type        | Null | Key | Default | Extra          |
+			+------------+-------------+------+-----+---------+----------------+
+			| identifier | int(11)     | NO   | PRI | NULL    | auto_increment |
+			| name       | varchar(20) | YES  |     | NULL    |                |
+			+------------+-------------+------+-----+---------+----------------+
+		*/
+		rows, err := db.QueryContext(ctx, "SELECT identifier, name FROM eda")
+		if err != nil {
+			return fmt.Errorf("could not get EDA definition: %w", err)
+		}
+		defer rows.Close()
+
+		nrows := 0
+		for rows.Next() {
+			var (
+				id   int32
+				name string
+			)
+			err = rows.Scan(&id, &name)
+			if err != nil {
+				return fmt.Errorf("could not scan EDA table: %w", err)
+			}
+			log.Printf("eda> id=%v, name=%v", id, name)
+			nrows++
+		}
+		if nrows == 0 {
+			log.Printf("empty eda table")
+		}
+	}
+
+	{
+		rows, err := db.QueryContext(ctx, "SELECT detector, layer, identifier, rfm, chamber, ix, iy, slot, hv, voltage FROM rfm")
+		if err != nil {
+			return fmt.Errorf("could not get RFM definition: %w", err)
+		}
+		defer rows.Close()
+		/*
+			| detector   | int(11)              | NO   | PRI | NULL
+			| layer      | int(11)              | NO   | PRI | NULL
+			| identifier | int(11)              | NO   | PRI | NULL
+			| rfm        | smallint(5) unsigned | NO   |     | 0
+			| rtl        | smallint(5) unsigned | NO   |     | 0
+			| chamber    | smallint(5) unsigned | NO   |     | 0
+			| ix         | tinyint(1) unsigned  | NO   |     | 0
+			| iy         | tinyint(1) unsigned  | NO   |     | 0
+			| slot       | tinyint(1) unsigned  | NO   |     | 0
+			| hv         | smallint(5) unsigned | NO   |     | 0
+			| voltage    | float                | NO   |     | 0
+		*/
+
+		nrows := 0
+		for rows.Next() {
+			var (
+				det     int32
+				layer   int32
+				id      int32
+				rfm     uint8
+				chamber uint8
+				ix      uint8
+				iy      uint8
+				slot    uint8
+				hv      uint32
+				volt    float64
+			)
+			err = rows.Scan(&det, &layer, &id, &rfm, &chamber, &ix, &iy, &slot, &hv, &volt)
+			if err != nil {
+				return fmt.Errorf("could not scan RFM table: %w", err)
+			}
+			log.Printf(
+				"rfm> id=%v, rfm=%v, slot=%v, ix=%v, iy=%v, det=%v, layer=%v, hv=%v, volt=%v",
+				id, rfm, slot, ix, iy, det, layer, hv, volt,
+			)
+			nrows++
+		}
+		if nrows == 0 {
+			log.Printf("empty rfm table")
+		}
+	}
 
 	daqstates, err := db.DAQStates(ctx)
 	if err != nil {
